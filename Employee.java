1fc6092ca331a7b63f0dac9f9432f1d9e7f5da8a
Tatyana <tatyana@gus.ru>
2023-12-24 06:03:26 +0300

    класс Employee

--- Employee.java
+++ Employee.java
@@ -8,54 +8,88 @@ public class Employee {
     private int age;
     private double salary;
 
-    public int getAge(){
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
         return age;
     }
 
-    public double getSalary(){
+    public double getSalary() {
         return salary;
     }
 
-    public String getFullName(){
+    public String getFullName() {
         return firstName + " " + lastName;
     }
 
-    public void setFirstName(String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void setLastName(String lastName){
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public void setSalary(double salary){
+    public void setSalary(double salary) {
         this.salary = salary;
     }
 
-    public Employee(String firstName, String lastName, int age, double salary){
+    public Employee(String firstName, String lastName, int age, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.salary = salary;
     }
 
-    public Employee(String firstName, String lastName, int age){
+    public Employee(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         salary = 1000;
     }
 
-    public Employee(String firstName, String lastName){
+    public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
         age = 25;
         salary = 1000;
     }
 
-    
+    @Override
+    public String toString() {
+        return "\nДанные о сотруднике:" +
+                "\nИмя -> " + firstName +
+                "\nФамилия -> " + lastName +
+                "\nВозраст -> " + age +
+                "\nЗаработная плата -> " + salary;
+    }
+
+    @Override
+    public boolean equals(Object anotherObject) {
+        if (anotherObject == null) {
+            return false;
+        }
+        if (!(anotherObject instanceof Employee)) {
+            return false;
+        }
+        Employee anotherEmployee = (Employee) anotherObject;
+        return firstName.equals(anotherEmployee.getFirstName()) && lastName.equals(anotherEmployee.getLastName())
+                && age == anotherEmployee.getAge() && salary == anotherEmployee.getSalary();
+    }
+
+    @Override
+    public int hashCode() {
+        return firstName.hashCode() + 7 * lastName.hashCode() + 13 * age;
+    }
+
 }
